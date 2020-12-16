216a56d065270f48914fe1fbd06cbd36a877e21d
Shehan Wijeyaweera <shehan.naveendra@gmail.com>
2020-12-16 21:02:22 +0530

    Admin side view All users Implemented with tax and licence image view.

--- src/main/java/com/springbootweb/bangercocarrental/Config/MvcConfig.java
+++ src/main/java/com/springbootweb/bangercocarrental/Config/MvcConfig.java
@@ -1,11 +1,13 @@
 package com.springbootweb.bangercocarrental.Config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+@Configuration
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
--- src/main/java/com/springbootweb/bangercocarrental/Controller/AdminController.java
+++ src/main/java/com/springbootweb/bangercocarrental/Controller/AdminController.java
@@ -1,6 +1,9 @@
 package com.springbootweb.bangercocarrental.Controller;
 
+import com.springbootweb.bangercocarrental.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin/")
 public class AdminController {
 
+    @Autowired
+    private UserRepository userRepository;
+
     @GetMapping("/homepage")
     public String admin_homepage(){
         return "admin_homepage";
+    }
+
+    @GetMapping("/users")
+    public String ViewAllUsers(Model model){
+        model.addAttribute("users", userRepository.findAll());
+        return "admin_viewAllUsers";
     }
 }
